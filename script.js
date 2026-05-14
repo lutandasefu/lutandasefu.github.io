531205eed5af1e548be8af71b054cd4fc80876ea
lutandasefu <cleolutandalizosefu@gmail.com>
2026-05-14 13:50:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -280,20 +280,3 @@ revealElements.forEach(function(el) {
   revealObserver.observe(el);
 });
 
-/* ===== HAMBURGER MENU ===== */
-var hamburger = document.getElementById('hamburger');
-var mobileMenu = document.getElementById('mobile-menu');
-
-if (hamburger && mobileMenu) {
-  hamburger.addEventListener('click', function() {
-    hamburger.classList.toggle('open');
-    mobileMenu.classList.toggle('open');
-  });
-
-  mobileMenu.querySelectorAll('a').forEach(function(link) {
-    link.addEventListener('click', function() {
-      hamburger.classList.remove('open');
-      mobileMenu.classList.remove('open');
-    });
-  });
-}
