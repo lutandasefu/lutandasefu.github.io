70063f39fbfcff89656607febc9c44318d042368
lutandasefu <cleolutandalizosefu@gmail.com>
2026-05-16 13:15:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -97,20 +97,6 @@ if (canvas) {
   drawDots();
 }
 
-/* ===== FONT PRELOAD ===== */
-document.fonts.ready.then(function() {
-  var heading = document.getElementById('hero-heading');
-  if (heading) heading.classList.add('font-ready');
-});
-
-/* fallback in case fonts.ready doesn't fire */
-setTimeout(function() {
-  var heading = document.getElementById('hero-heading');
-  if (heading && !heading.classList.contains('font-ready')) {
-    heading.classList.add('font-ready');
-  }
-}, 1000);
-
 
 /* ===== NAVBAR HIDE/SHOW ON SCROLL ===== */
 var navbar = document.getElementById('custom-navbar');
